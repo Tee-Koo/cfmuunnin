f8918d05877a8e1cee8e0495591c640f4b6396db
Tee-Koo <tero.korpi@gmail.com>
2021-09-13 18:41:13 +0300

    JS

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let celsius = document.getElementById("celsius");
 let fahrenheit = document.getElementById("fahrenheit");
 
-function celToFar(){
+function cel(){
     let output = ( parseFloat(celsius.value) * 9/5 ) + 32;
     fahrenheit.value = parseFloat(output.toFixed(2));
 }
 
-function farToCel(){
+function far(){
     let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
     celsius.value = parseFloat( output.toFixed(2));
-    console.log(output);
 }
